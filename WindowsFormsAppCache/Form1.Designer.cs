febbad8e33ea85e8412e37440b086284596bbad9
Mihail Salkov <misha-sal@mail.ru>
2021-09-06 00:00:30 +0500

    Fix window size in WindowsFormsAppCache

--- WindowsFormsAppCache/Form1.Designer.cs
+++ WindowsFormsAppCache/Form1.Designer.cs
@@ -92,7 +92,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(294, 128);
+            this.ClientSize = new System.Drawing.Size(294, 98);
             this.Controls.Add(this.buttonPut);
             this.Controls.Add(this.buttonGet);
             this.Controls.Add(this.labelValue);
